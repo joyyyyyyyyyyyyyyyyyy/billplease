c38e62a5450a755a7d0114d14cef691991ea1cd7
joyyyyyyyyyyyyyyyyyy <22022096@myrp.edu.sg>
2023-05-11 00:05:42 +0800

    billplease

--- app/src/main/java/sg/edu/rp/c346/id22022096/billplease/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22022096/billplease/MainActivity.java
@@ -57,20 +57,23 @@ public class MainActivity extends AppCompatActivity {
 
           //discount
             if (disc.getText().toString().trim().length() !=0) {
-              newamt = 1 - Double.parseDouble(disc.getText().toString()) / 100;
+                double originalamt = Double.parseDouble(amt.getText().toString());
+                newamt = originalamt * (1-Double.parseDouble(disc.getText().toString()) / 100);
           }
 
-            String type = "in cash";
+            String method = " in cash";
             if (payment.getCheckedRadioButtonId() == R.id.radiobtnpaynow) {
-              type = " via PayNow to 9123 4567";
-          }
+                method = " via PayNow to 9123 4567";
+            }
 
-          bill.setText("$" + String.format("%.2f", newamt));
-            int totalperson = Integer.parseInt(paxno.getText().toString());
+          int totalperson = Integer.parseInt(paxno.getText().toString());
+          double splitamt = newamt/totalperson;
+
+          bill.setText("Total BIll Amount: $" + String.format("%.2f", newamt));
             if (totalperson !=1)
-                payable.setText("$" + String.format("%.2f", newamt));
+                payable.setText("Each Person Pays: $" + String.format("%.2f", splitamt) + method);
             else
-                payable.setText("$" + newamt + type);
+                payable.setText("$" + newamt + method);
         });
 
         reset.setOnClickListener(v -> {
